29b0afca089a929fa16ffd6e329ae19e16f0c50e
Manuel Maly <manuel.maly@gmail.com>
2019-01-06 21:30:05 +0100

    Fix comments in IdiomaticListViewDatasourceCore.

--- DataSourcerer/Classes/List/IdiomaticListViewDatasourceCore.swift
+++ DataSourcerer/Classes/List/IdiomaticListViewDatasourceCore.swift
@@ -67,18 +67,15 @@ public struct IdiomaticListViewDatasourceCore
 public extension IdiomaticListViewDatasourceCore {
 
     /// Configures standard components and assumes standard behavior that might be suitable
-    /// for most "normal" UITableView use cases:
-    ///   - Cached datasource is required (which can also be instantiated without a cache BTW)
-    ///   - A response container is shown from which Items are retrieved (configurable via closure)
-    ///   - Pull to refresh is enabled (might be configurable later on)
-    ///   - When loading, a UIActivityIndicatorView is shown in a item
-    ///   - If an error occurs, a specific item is shown
-    ///   - If no results are visible, a specific item is shown
-    ///   - Items are either selectable or not
-    ///   - TableView updates are animated if the view is visible
-    ///
-    /// Usage: Instantiate and configure with the offered parameters and functions and add the
-    /// `tableViewController` to the view hierarchy.
+    /// for most "normal" list view use cases:
+    ///   - A response container (=value) is shown from which Items are retrieved
+    ///       (configurable via closure)
+    ///   - Pull to refresh is enabled (should become configurable later on)
+    ///   - When loading, a loading cell is shown
+    ///   - If an error occurs, an error item is shown
+    ///   - If no results are visible, a "no results" item is shown
+    ///   - Items can be selectable
+    ///   - TableView updates are animated
     struct Builder {
         public typealias Core = IdiomaticListViewDatasourceCore
 
